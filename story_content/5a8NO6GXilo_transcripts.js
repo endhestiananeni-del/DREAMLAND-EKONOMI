04077f58274741681377bd9bcb01ddbc8222c795
endhestiananeni-del <endhestiananeni@gmail.com>
2026-05-12 04:21:13 +0700

    APLOUD

---
+++ story_content/5a8NO6GXilo_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":5349,"text":"Let's cooperate in trade Yes for mutual benefit."},{"start":9699,"text":"Jabat tangan tersebut adalah awal dari"},{"start":11859,"text":"berbagai bentuk kerjasama ekonomi di dunia."},{"start":14659,"text":"Dalam kerjasama ekonomi ada dua bentuk utama."},{"start":17739,"text":"Bilateral adalah kerjasama antara dua negara aja."},{"start":20700,"text":"Contohnya Indonesia dengan Jepang dalam perdagangan atau investasi"},{"start":24620,"text":"multilateral adalah kerjasama yang melibatkan banyak negara,"},{"start":28139,"text":"biasanya melalui organisasi internasional seperti WBOO atau IMF."}]}]};
+    window.globalLoadJsAsset('story_content/5a8NO6GXilo_transcripts.js', JSON.stringify(data));
+})();
